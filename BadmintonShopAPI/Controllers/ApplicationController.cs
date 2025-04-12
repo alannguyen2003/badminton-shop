2153dc3132bf59b729742e6efbc581fe04ab0b24
alannguyen2003 <nguyenho30112003@gmail.com>
2025-04-12 14:45:12 +0700

    feat: docker compose for api service

---
+++ BadmintonShopAPI/Controllers/ApplicationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BadmintonShopAPI.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+public class ApplicationController: ControllerBase
+{
+    public ApplicationController()
+    {
+    }
+
+    [HttpGet("get-version")]
+    public async Task<IActionResult> GetVersion()
+    {
+        return Ok(1);
+    }
+}
